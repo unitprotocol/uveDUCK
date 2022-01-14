f74de91bc5eede575df61149a3757d8b91c2d32c
pum <pum@purum>
2022-01-14 04:25:03 +0300

    stored stashv3 in repo + test for gaugev3+stashv3

--- test/test.js
+++ test/test.js
@@ -3,12 +3,16 @@ const { solidity } = require("ethereum-waffle")
 chai.use(solidity)
 const { ethers } = require('hardhat')
 const {deploy} = require("../scripts/deployFns");
+const {expect} = require("chai");
 
 const DAY = 86400
 const WEEK = 7 * DAY
 
-const crvWhale = '0x7a16fF8270133F063aAb6C9977183D9e72835428'
-const usdpLPWhale = '0x1b5eb1173d2bf770e50f10410c9a96f7a8eb6e75'
+const USDP_POOL_ID = 0;
+const CVXCRV_POOL_ID = 1;
+
+const crvWhaleAddress = '0x7a16fF8270133F063aAb6C9977183D9e72835428'
+const usdpLPWhaleAddress = '0x1b5eb1173d2bf770e50f10410c9a96f7a8eb6e75'
 const usdpAddress = '0x1456688345527be1f37e9e627da0837d6f08c925'
 const wcd = '0x40907540d8a6c65c637785e8f8b742ae6b0b9968'
 const sww = '0xca719728Ef172d0961768581fdF35CB116e0B7a4'
@@ -17,8 +21,17 @@ const usdpGauge = '0x055be5DDB7A925BfEF3417FC157f53CA77cA7222'
 const extraRewardStashV3 = '0xd7AbC64CAFc30FDd08A42Ea4bC13846be455399C'
 const unitVault = '0xb1cFF81b9305166ff1EFc49A129ad2AfCd7BCf19'
 
-let crv, admin, alice, bob, charlie, m, walletCheckerDao,
-    smartWalletWhiteList, u, usdpLP, threecrv, dai, usdp
+// v3 gauge
+const cvxcrvGaugeAddress = '0x903da6213a5a12b61c821598154efad98c3b20e4';
+const cvxcrvLPAddress = '0x9d0464996170c6b9e75eed71c68b99ddedf279e8';
+const cvxcrvLPWhaleAddress = '0x903da6213a5a12b61c821598154efad98c3b20e4';
+const cvxAddress = '0x4e3fbd56cd56c3e72c1403e103b45db9da5b9d2b';
+const cvxWhaleAddress = '0x5f465e9fcffc217c5849906216581a657cd60605';
+const cvxcrvGaugeAdminAddress = '0x2ef1bc1961d3209e5743c91cd3fbfa0d08656bc3'
+
+let crv, admin, alice, bob, charlie, crvWhale, walletCheckerDao,
+    smartWalletWhiteList, usdpLPWhale, usdpLP, threecrv, dai, usdp,
+    cvxcrvLP, cvxcrvLPWhale, cvx, cvxWhale, cvxcrvGaugeAdmin, cvxcrvGauge
 
 let voteProxy, booster, uveCrv, rewardFactory,
     stashFactory, extraRewardStashV2, tokenFactory,
@@ -44,11 +57,11 @@ describe("Functional test", function() {
     wrappedToUnderlyingOracle = await ethers.getContractAt("IWrappedToUnderlyingOracle", '0x220Ea780a484c18fd0Ab252014c58299759a1Fbd')
     unitParameters = await ethers.getContractAt("IParametersBatchUpdater", '0x4DD1A6DB148BEcDADAdFC407D23b725eDd3cfB6f')
 
-    await ethers.provider.send("hardhat_impersonateAccount", [crvWhale]);
-    await ethers.provider.send("hardhat_impersonateAccount", [usdpLPWhale]);
-    m = await ethers.getSigner(crvWhale)
-    u = await ethers.getSigner(usdpLPWhale)
-    await ethers.provider.send("hardhat_setBalance", [usdpLPWhale, '0x3635c9adc5dea00000' /* 1000Ether */]);
+    await ethers.provider.send("hardhat_impersonateAccount", [crvWhaleAddress]);
+    await ethers.provider.send("hardhat_impersonateAccount", [usdpLPWhaleAddress]);
+    crvWhale = await ethers.getSigner(crvWhaleAddress)
+    usdpLPWhale = await ethers.getSigner(usdpLPWhaleAddress)
+    await ethers.provider.send("hardhat_setBalance", [usdpLPWhaleAddress, '0x3635c9adc5dea00000' /* 1000Ether */]);
 
     await ethers.provider.send("hardhat_impersonateAccount", ['0xae37E8f9a3f960eE090706Fa4db41Ca2f2C56Cb8']);
     unitMultisig = await ethers.getSigner("0xae37E8f9a3f960eE090706Fa4db41Ca2f2C56Cb8")
@@ -71,18 +84,37 @@ describe("Functional test", function() {
     );
 
     lockFees = await ethers.getContractAt("FeePool", await booster.lockFees())
+
+    // cvxCRV for gaugev3
+    cvxcrvLP = await ethers.getContractAt("IERC20", cvxcrvLPAddress)
+    await ethers.provider.send("hardhat_impersonateAccount", [cvxcrvLPWhaleAddress]);
+    cvxcrvLPWhale = await ethers.getSigner(cvxcrvLPWhaleAddress)
+    await ethers.provider.send("hardhat_setBalance", [cvxcrvLPWhaleAddress, '0x3635c9adc5dea00000' /* 1000Ether */]);
+
+    cvx = await ethers.getContractAt("IERC20", cvxAddress)
+    await ethers.provider.send("hardhat_impersonateAccount", [cvxWhaleAddress]);
+    cvxWhale = await ethers.getSigner(cvxWhaleAddress)
+    await ethers.provider.send("hardhat_setBalance", [cvxWhaleAddress, '0x3635c9adc5dea00000' /* 1000Ether */]);
+
+    await ethers.provider.send("hardhat_impersonateAccount", [cvxcrvGaugeAdminAddress]);
+    cvxcrvGaugeAdmin = await ethers.getSigner(cvxcrvGaugeAdminAddress)
+    await ethers.provider.send("hardhat_setBalance", [cvxcrvGaugeAdminAddress, '0x3635c9adc5dea00000' /* 1000Ether */]);
+
+    cvxcrvGauge = await ethers.getContractAt("ICurveGauge", cvxcrvGaugeAddress)
+
+    await booster.addPool(cvxcrvLPAddress, cvxcrvGaugeAddress, 3) // pool for cvxCRV
   })
 
   it("CRV -> UVECRV", async function() {
 
-    await crv.connect(m).transfer(voteProxy.address, 1) // transfers on voteproxy some crv for initial lock
+    await crv.connect(crvWhale).transfer(voteProxy.address, 1) // transfers on voteproxy some crv for initial lock
 
     await smartWalletWhiteList.connect(walletCheckerDao).approveWallet(voteProxy.address)
 
     await crvDepositor.initialLock()
-    await crv.connect(m).approve(crvDepositor.address, crvDepositAmount)
+    await crv.connect(crvWhale).approve(crvDepositor.address, crvDepositAmount)
 
-    const depositResult1 = await crvDepositor.connect(m)['deposit(uint256,address)'](crvDepositAmount, lockFees.address)
+    const depositResult1 = await crvDepositor.connect(crvWhale)['deposit(uint256,address)'](crvDepositAmount, lockFees.address)
     console.log('Deposit gas used', (await depositResult1.wait()).gasUsed.toString())
   })
 
@@ -101,9 +133,9 @@ describe("Functional test", function() {
     await unitParameters.connect(unitMultisig).setCollaterals([lockFees.address], 0, 0, 50, 75, 0, 1100, 10n ** 23n, [11])
 
     // provide collateral and mint some USDP
-    await lockFees.connect(m).approve(unitVault, crvDepositAmount)
-    await usdp.connect(m).approve(cdpManager.address, 10n ** 20n)
-    await cdpManager.connect(m).join(lockFees.address, crvDepositAmount, 10n ** 21n)
+    await lockFees.connect(crvWhale).approve(unitVault, crvDepositAmount)
+    await usdp.connect(crvWhale).approve(cdpManager.address, 10n ** 20n)
+    await cdpManager.connect(crvWhale).join(lockFees.address, crvDepositAmount, 10n ** 21n)
 
     // await booster.setRewardContract(ethers.constants.AddressZero /*veDistribution*/);
 
@@ -167,19 +199,19 @@ describe("Functional test", function() {
 
     await threecrv.balanceOf(lockFees.address).then( a => console.log("lockFees balance: ", BigInt(a)))
 
-    await lockFees.earned(crvWhale).then(a => console.log("earned fees: ", BigInt(a)))
+    await lockFees.earned(crvWhaleAddress).then(a => console.log("earned fees: ", BigInt(a)))
 
     console.log("advance time...");
     await ethers.provider.send('evm_mine', [await chainTime() + DAY])
-    await lockFees.earned(crvWhale).then(a => console.log("earned fees: ", BigInt(a)))
+    await lockFees.earned(crvWhaleAddress).then(a => console.log("earned fees: ", BigInt(a)))
 
     console.log("advance time...");
     await ethers.provider.send('evm_mine', [await chainTime() + DAY])
-    await lockFees.earned(crvWhale).then(a => console.log("earned fees: ", BigInt(a)))
+    await lockFees.earned(crvWhaleAddress).then(a => console.log("earned fees: ", BigInt(a)))
 
-    const balanceBefore = BigInt(await threecrv.balanceOf(crvWhale))
-    await lockFees.connect(m)['getReward()']()
-    const balanceAfter = BigInt(await threecrv.balanceOf(crvWhale))
+    const balanceBefore = BigInt(await threecrv.balanceOf(crvWhaleAddress))
+    await lockFees.connect(crvWhale)['getReward()']()
+    const balanceAfter = BigInt(await threecrv.balanceOf(crvWhaleAddress))
     console.log('reward claimed', balanceAfter - balanceBefore, '3CRV')
   })
 
@@ -187,14 +219,13 @@ describe("Functional test", function() {
     const depositAmount = 10n ** 21n
 
     // stake usdpCRV
-    await usdpLP.connect(u).approve(booster.address, depositAmount)
-    await booster.connect(u).deposit(0, depositAmount, true)
+    await usdpLP.connect(usdpLPWhale).approve(booster.address, depositAmount)
+    await booster.connect(usdpLPWhale).deposit(USDP_POOL_ID, depositAmount, true)
     console.log('usdpCRV deposited & staked')
   })
 
   it("CRV claim for lp staking", async function() {
-    const poolId = 0
-    const poolinfo = await booster.poolInfo(poolId)
+    const poolinfo = await booster.poolInfo(USDP_POOL_ID)
     const rewardPoolAddress = poolinfo.crvRewards
     const rewardPool = await ethers.getContractAt("BaseRewardPool", rewardPoolAddress)
     const depositToken = await ethers.getContractAt("DepositToken", poolinfo.token)
@@ -209,26 +240,100 @@ describe("Functional test", function() {
     await ethers.provider.send('evm_mine', [await chainTime() + DAY])
 
     //collect and distribute rewards off gauge
-    await booster.earmarkRewards(poolId)
+    await booster.earmarkRewards(USDP_POOL_ID)
     console.log("----earmark 1----")
 
-    await rewardPool.earned(usdpLPWhale).then(a => console.log("claimable", BigInt(a), 'CRV'))
+    await rewardPool.earned(usdpLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
 
     console.log("advance time...")
     await ethers.provider.send('evm_mine', [await chainTime() + 6 * DAY])
 
-    await booster.earmarkRewards(poolId)
+    await booster.earmarkRewards(USDP_POOL_ID)
     console.log("----earmark 2----")
 
-    await rewardPool.earned(usdpLPWhale).then(a => console.log("claimable", BigInt(a), 'CRV'))
+    await rewardPool.earned(usdpLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
 
     await crv.balanceOf(rewardPool.address).then(a=>console.log("crv at reward pool", BigInt(a)))
 
     console.log("advance time...")
     await ethers.provider.send('evm_mine', [await chainTime() + 7 * DAY])
-    await rewardPool.earned(usdpLPWhale).then(a => console.log("claimable", BigInt(a), 'CRV'))
-    await rewardPool.connect(u)['getReward()']()
-    await crv.balanceOf(usdpLPWhale).then(a=>console.log("claimed", BigInt(a), 'CRV'))
+    await rewardPool.earned(usdpLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
+    await rewardPool.connect(usdpLPWhale)['getReward()']()
+    await crv.balanceOf(usdpLPWhaleAddress).then(a=>console.log("claimed", BigInt(a), 'CRV'))
+  })
+
+
+  it("lp staking v3", async function() {
+    const depositAmount = 1000n * 10n ** 18n
+
+    // stake cvxCRV
+    await cvxcrvLP.connect(cvxcrvLPWhale).approve(booster.address, depositAmount)
+    await booster.connect(cvxcrvLPWhale).deposit(CVXCRV_POOL_ID, depositAmount, true)
+    console.log('cvxcrv deposited & staked')
+  })
+
+  it("CRV+additional tokens claim for lp staking v3", async function() {
+    const poolinfo = await booster.poolInfo(CVXCRV_POOL_ID)
+    const rewardPoolAddress = poolinfo.crvRewards
+    const stashV3Address = poolinfo.stash
+    const rewardPool = await ethers.getContractAt("BaseRewardPool", rewardPoolAddress)
+    const depositToken = await ethers.getContractAt("DepositToken", poolinfo.token)
+
+    expect(poolinfo.lptoken.toLowerCase()).to.be.equal(cvxcrvLPAddress)
+    expect(poolinfo.gauge.toLowerCase()).to.be.equal(cvxcrvGaugeAddress)
+
+    console.log("pool lp token " +poolinfo.lptoken)
+    console.log("pool gauge " +poolinfo.gauge)
+    console.log("pool reward contract at " +rewardPool.address)
+    console.log("stash v3 contract at " + stashV3Address)
+
+    let crvBalance = await crv.balanceOf(cvxcrvLPWhaleAddress)
+    let cvxBalance = await cvx.balanceOf(cvxcrvLPWhaleAddress)
+    expect(BigInt(cvxBalance)).to.be.equal(0n)
+    console.log('before all rewards manipulations: CRV:', BigInt(crvBalance), 'CVX:', BigInt(cvxBalance))
+
+    await depositToken.name().then(a=>console.log("deposit token name: " +a))
+    await depositToken.symbol().then(a=>console.log("deposit token symbol: " +a))
+
+    // add reward in cvx token
+    expect(await rewardPool.extraRewardsLength()).to.be.equal(0);
+    await cvxcrvGauge.connect(cvxcrvGaugeAdmin).add_reward(cvxAddress, cvxWhaleAddress);
+    await cvx.connect(cvxWhale).approve(cvxcrvGauge.address, 1000n * 10n ** 18n)
+    await cvxcrvGauge.connect(cvxWhale).deposit_reward_token(cvxAddress, 1000n * 10n ** 18n)
+    expect(await rewardPool.extraRewardsLength()).to.be.equal(0);
+
+    await ethers.provider.send('evm_mine', [await chainTime() + DAY])
+
+    //collect and distribute rewards off gauge
+    await booster.earmarkRewards(CVXCRV_POOL_ID)
+    // added additional token for rewards
+    expect(await rewardPool.extraRewardsLength()).to.be.equal(1);
+    const extraRewardPool = await ethers.getContractAt('VirtualBalanceRewardPool', await rewardPool.extraRewards(0));
+    expect((await extraRewardPool.rewardToken()).toLowerCase()).to.be.equal(cvxAddress);
+    console.log("----earmark 1----")
+
+    await rewardPool.earned(cvxcrvLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
+
+    console.log("advance time...")
+    await ethers.provider.send('evm_mine', [await chainTime() + 6 * DAY])
+
+    await booster.earmarkRewards(CVXCRV_POOL_ID)
+    console.log("----earmark 2----")
+
+    await rewardPool.earned(cvxcrvLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
+
+    await crv.balanceOf(rewardPool.address).then(a=>console.log("crv at reward pool", BigInt(a)))
+    await cvx.balanceOf(rewardPool.address).then(a=>console.log("cvx at reward pool", BigInt(a)))
+
+    console.log("advance time...")
+    await ethers.provider.send('evm_mine', [await chainTime() + 7 * DAY])
+    await rewardPool.earned(cvxcrvLPWhaleAddress).then(a => console.log("claimable", BigInt(a), 'CRV'))
+    await rewardPool.connect(cvxcrvLPWhale)['getReward()']()
+
+    crvBalance = await crv.balanceOf(cvxcrvLPWhaleAddress)
+    cvxBalance = await cvx.balanceOf(cvxcrvLPWhaleAddress)
+    expect(BigInt(cvxBalance) > 0n).to.be.true
+    console.log('claimed: CRV:', BigInt(crvBalance), 'CVX:', BigInt(cvxBalance))
   })
 })
 
